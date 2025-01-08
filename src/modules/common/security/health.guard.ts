787227d3df502c8c7125da843ce1da5d6eb626a5
binaryYuki <60097976+binaryYuki@users.noreply.github.com>
2025-01-08 20:11:52 +0000

    typo: rename HEALTH_TOKEN to HEALTH_CHECK_TOKEN
    Signed-off-by: binaryYuki <60097976+binaryYuki@users.noreply.github.com>

--- src/modules/common/security/health.guard.ts
+++ src/modules/common/security/health.guard.ts
@@ -7,6 +7,6 @@ export class HealthGuard implements CanActivate {
     public canActivate(context: ExecutionContext): boolean {
 
         const request = context.switchToHttp().getRequest<FastifyRequest>();
-        return request.headers.authorization === `Bearer ${process.env.HEALTH_TOKEN}`;
+        return request.headers.authorization === `Bearer ${process.env.HEALTH_CHECK_TOKEN}`;
     }
 }
