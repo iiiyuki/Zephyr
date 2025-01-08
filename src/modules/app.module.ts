2bc06cd46b5612f72292395f4b6f0e056f22b359
binaryYuki <60097976+binaryYuki@users.noreply.github.com>
2025-01-08 00:57:10 +0000

    feat: initial commit
    Signed-off-by: binaryYuki <60097976+binaryYuki@users.noreply.github.com>

---
+++ src/modules/app.module.ts
@@ -0,0 +1,13 @@
+import {Module} from '@nestjs/common';
+
+import {CommonModule} from './common';
+import {PassengerModule} from './passenger/passenger.module';
+
+@Module({
+    imports: [
+        CommonModule,
+        PassengerModule
+    ]
+})
+export class ApplicationModule {
+}
---
+++ src/modules/passenger/model/passenger.input.ts
@@ -0,0 +1,5 @@
+import {PickType} from '@nestjs/swagger';
+import {PassengerData} from './passenger.data';
+
+export class PassengerInput extends PickType(PassengerData, ['firstName', 'lastName'] as const) {
+}
